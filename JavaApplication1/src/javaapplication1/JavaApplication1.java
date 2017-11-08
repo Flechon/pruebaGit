f41f1b1b02b66c55bd22e59bac809429a6ea6992
Cristian.M.C <cmontero.capo@gmail.com>
2017-11-08 16:30:46 -0300

    noseqwea

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,7 +3,12 @@ package javaapplication1;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         
+=======
       int mientero;
+        // TODO code application logic here
+>>>>>>> 750bcd7702b277e42ce5f3bf2c26a61974299ce5
     }
+
 }
